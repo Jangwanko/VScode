af10384185065d321b46bd415399ebc55623fa09
jangwanko <jangwanko93@gmail.com>
2022-05-19 01:47:41 +0900

    0519

--- python/hello.py
+++ python/hello.py
@@ -1,2 +1,10 @@
-print('hello')
-print('i want get a job')
+'''num=int(input())
+
+for i in range(1, num+1):
+    num_list=list(map(int,input().split()))
+    sum_count=0
+    for j in num_list:
+        sum_count+=j
+        ans=sum_count/len(num_list)
+    print(f"#{i} {round(ans)}")'''
+
