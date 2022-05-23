2b712a1cc34babc2ecf6460eeb0172551c8b4c16
jangwanko <jangwanko93@gmail.com>
2022-05-24 02:41:30 +0900

    0524

--- python/main.py
+++ python/main.py
@@ -1,31 +1,14 @@
-num = int(input())
-for i in range(1, num + 1):
+T = int(input())
 
-    a = 0
-    b = 0
-    c = 0
-    d = 0
-    e = 0
-    count = int(input())
-    if count / 11 != 0:
-        e = count / 11
-        if e / 7 != 0:
-            d = e / 7
-            if d / 5 != 0:
-                c = d / 5
-                if c / 3 != 0:
-                    b = c / 3
-                    if b / 2 != 0:
-                        a = b / 2
-                    else:
-                        a = 0
-                else:
-                    b = 0
-            else:
-                c = 0
+for i in range(1, T + 1):
+    n = int(input())
+    day = list(map(int, input().split()))
+    day = day[::-1]
+    daysum = 0
+    result = day[0]
+    for j in range(1, n):
+        if result > day[j]:
+            daysum = daysum + result - day[j]
         else:
-            d = 0
-    else:
-        e = 0
-
-    print(f"#{i} {int(a)} {b} {c} {d} {e}")
+            result = day[j]
+    print(f"#{i} {daysum}")
